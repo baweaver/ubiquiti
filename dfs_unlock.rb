71db57b46f25e5b6d0b409b98cdfe36d0af5e7fe
Brandon Weaver <brandon_weaver@baweaver.com>
2013-06-13 11:28:29 -0500

    Added Single IP DFS Unlocker

---
+++ dfs_unlock.rb
@@ -0,0 +1,23 @@
+# DFS Unlocker for a single IP. Can easily be made to run on multiple units
+# and I'll be updating it to do so some time later.
+
+$DFS_Company = ''
+$DFS_Key = ''
+
+$su_user = 'ubnt'
+$su_pass = 'ubnt'
+
+ip = ARGV[0]
+
+def dfsunlock(ip)
+  begin
+    Net::SSH.start(ip, $su_user, :password => $su_pass, :paranoid => false) do |ssh|
+      puts "[ ] Unlocking DFS"
+      puts "\t" +ssh.exec!("/bin/ubntbox dfs-unlock '#{$DFS_Company}' '#{$DFS_Key}'")
+    end
+  rescue
+    puts "[X] Failed to unlock!"
+  end
+end
+
+dfsunlock(ip)
